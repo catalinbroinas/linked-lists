ac38cd98fff03f14e321beaab6e2b098bcb0b7f0
catalinbroinas <catalinbroinas@yahoo.com>
2024-07-23 16:41:53 +0300

    Add `getList` method to `LinkedList`

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 // Create a linked list
 function LinkedList() {
+    // Define an empty list
+    const list = {
+        head: null
+    };
 
+    // Get the entire list
+    const getList = () => list;
+
+    return { getList }
 }
 
 // Create a node
